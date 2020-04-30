52ec97076adcd0c88a2e6ed147d83d455ad5994b
galbimandu <mkim525notes@gmail.com>
2020-04-30 15:23:12 +0900

    create blog develop branch

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://galbimandu.github.io/',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
+  title: "Galbimandu's Blog",
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
